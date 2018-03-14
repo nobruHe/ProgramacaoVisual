ba9bc60cd2f850cf56993cea6ce3b145c045f0ca
BrunoH <bhenrique403@gmail.com>
2018-03-14 01:23:45 +0000

    Add Index

--- Source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
+++ Source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
@@ -30,5 +30,23 @@ namespace Web.Mvc.Controllers
                 return View(viewModel);
 
             }
+
+            public IActionResult Create()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public IActionResult Create([Bind("Nome")]Pessoa newPessoa)
+            {
+                if(!ModelState.IsValid)
+                    return View(newPessoa);
+
+                newPessoa.Id = pessoaList.Max(p => p.Id) + 1;
+                pessoaList.Add(newPessoa);
+                
+                return RedirectToAction(nameof(Index));
+            }
         }
 }
